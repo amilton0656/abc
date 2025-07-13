caa90b566f4fd79f2c3df76782e488be6c135c75
unknown <amilton0656@gmail.com>
2025-07-13 14:43:25 -0300

    xx

--- setup/settings.py
+++ setup/settings.py
@@ -1,5 +1,6 @@
-
 from pathlib import Path
+from decouple import config, Csv
+from dj_database_url import  parse as db_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,25 +11,37 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l9sf8(^kdnje1d6g3pym*s@v2x##c9(c6)8p&25oegs(5$u=7('
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = ['191.252.109.21', 'amilton.com.br', 'localhost']
+ALLOWED_HOSTS = [
+    'localhost'
+]   #config("ALLOWED_HOSTS", cast=Csv)
 
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS =[
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aaa',
 ]
+
+THIRD_PARTY_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
+]
+
+MY_APPS = [
+    'todos.apps.TodosConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -64,11 +77,13 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': config(
+        "DATABASE_URL", 
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}", 
+        cast=db_url
+        )
     }
-}
+
 
 
 # Password validation
@@ -105,10 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- setup/urls.py
+++ setup/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from aaa import views
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView, TodoCompleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('aaa/', views.aaa),
-    path('bbb/', views.bbb),
-    path('ccc/', views.ccc),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create/", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
+    path("complete/<int:pk>", TodoCompleteView.as_view(), name='todo_complete')
 ]
